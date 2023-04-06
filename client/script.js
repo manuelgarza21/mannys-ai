950b0fd65926229e88c0628541c4946e8cd34f09
manuelgarza21 <60672816+manuelgarza21@users.noreply.github.com>
2023-04-06 19:02:16 -0400

    adding server

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from the server for bot response
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://mannys-ai.onrender.com',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
